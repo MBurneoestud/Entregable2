df508b2483f260311e16c4b08a854c93d400832c
MBurneoestud <mburneor@estud.usfq.edu.ec>
2026-09-16 19:03:42 -0500

    Completado reset, no se guardo edit a apiError.ts anteriormente

--- src/apiError.ts
+++
@@ -1,9 +0,0 @@
-export class ApiError extends Error {
-    public readonly status: number;
-
-    constructor(status: number, message: string) {
-        super(message);
-        this.status = status;
-        this.name = "ApiError";
-    }
-}
